47515f08886a7370b03d8f95897465ccbdf4741d
francesco <fralette@gmail.com>
2021-11-09 16:27:39 +0100

    rimozione routes

--- src/UIServiceProvider.php
+++ src/UIServiceProvider.php
@@ -16,7 +16,7 @@ class UIServiceProvider extends ServiceProvider
         // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'xelection');
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'xelection');
         // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-         $this->loadRoutesFrom(__DIR__.'/routes.php');
+       //  $this->loadRoutesFrom(__DIR__.'/routes.php');
 
         // Publishing is only necessary when using the CLI.
         if ($this->app->runningInConsole()) {
